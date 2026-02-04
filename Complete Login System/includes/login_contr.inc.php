906ba2d943e2a300f870025b66e5022c4ef0d9eb
Aniket Shelke <aniketshelke8487@gmail.com>
2026-02-04 14:49:27 +0530

    .

---
+++ Complete Login System/includes/login_contr.inc.php
@@ -0,0 +1,36 @@
+<?php
+
+
+declare(strict_types=1);
+
+function is_input_empty(string $username ,string $pwd) {
+
+      if(empty($username ) || empty($pwd )  ){
+
+             return true;
+      }else{
+           return false;
+      }
+
+}
+
+function is_username_present(bool|array $result){
+
+     if(!$result){
+     return true;
+     }else{
+        return false;
+     }
+
+}
+function is_pwd_wrong(string $pwd , string $hashedPwd){
+
+     if(!password_verify($pwd, $hashedPwd)){
+     return true;
+     }else{
+        return false;
+     }
+
+}
+
+
